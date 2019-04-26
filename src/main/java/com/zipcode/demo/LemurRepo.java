853376bae1e8d716ebd907eed1316a3265232970
zipcodeFrancisco <zipcodefrancisco@gmail.com>
2019-04-26 13:17:30 -0400

    1st commit

--- src/main/java/com/zipcode/demo/LemurRepo.java
+++ src/main/java/com/zipcode/demo/LemurRepo.java
@@ -7,5 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 @RepositoryRestResource(collectionResourceRel = "lemurs", path = "lemurs")
 public interface LemurRepo extends PagingAndSortingRepository<Lemur, Long> {
-    
+
 }
